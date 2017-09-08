21f5df9221738933dccc201ede9270c3f84afbbb
Caique Marques <mqcaique@gmail.com>
2017-09-07 22:13:02 -0300

    .vimbo/install.sh: small fix

--- .vimbo/install.sh
+++ .vimbo/install.sh
@@ -1,10 +1,9 @@
-dir="$HOME/.vim/bundle/Vundle.vim"
+vdir="$HOME/.vim/bundle/Vundle.vim"
 
 cp .vimrc $HOME/.vimrc
 
-if [ ! -n "${dir}" ]; then
-    git clone https://github.com/VundleVim/Vundle.vim ${dir}
+if [ ! -d "${vdir}" ]; then
+    git clone https://github.com/VundleVim/Vundle.vim ${vdir}
 fi
 
 vim +PluginInstall
-
